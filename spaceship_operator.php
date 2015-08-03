2a3f36e49837dda36d74db1a92cceda416c10d12
aed <aed@localhost.localdomain>
2015-08-02 23:34:46 -0700

    2015-08-03 DB: added spaceship operator example

---
+++ spaceship_operator.php
@@ -0,0 +1,20 @@
+<?php
+echo 'Inputs: ' . PHP_EOL;
+var_dump($argv);
+$a = (isset($argv[1])) ? (float) $argv[1] : 0;
+$b = (isset($argv[2])) ? (float) $argv[2] : 0;
+switch ($a <=> $b) {
+    case -1 :
+        echo "$a is less than $b";
+        break;
+    case 0 :
+        echo "$a is equal to $b";
+        break;
+
+    case 1 :
+        echo "$a is greater than $b";
+        break;
+    default :
+        echo 'Unknown';
+}
+echo PHP_EOL;
