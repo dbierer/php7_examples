607b4f54c1f5f9b37dfe501de6bdaa79c9b10d3d
dbierer <doug@unlikelysource.com>
2015-11-23 17:47:14 +0700

    2015-11-23 DB: preg_replace_array #4

--- preg_replace_callback_array.php
+++ preg_replace_callback_array.php
@@ -13,22 +13,19 @@ foreach($objects as $name => $fileObj){
     if ($fileObj->isFile()) {
         $contents = file($name);
         echo 'Processing: ' . $name . PHP_EOL;
-        $flag = FALSE;
         preg_replace_callback_array(
             [
                 // 1   2                 3          4        5
                 "!(.*)(preg_replace)\(\'(.*?\/e)\',(\".*?\")(.*)!" =>
-                function ($match) use ($flag) {
-                    if ($flag) {
-                        echo 'OLD: ' . PHP_EOL . trim($match[0]) . PHP_EOL;
-                        $replace = $match[1]
-                                . "preg_replace_callback('"
-                                . substr($match[3], 0, -1)
-                                . "',"
-                                . 'function ($a) { return "\'" . chr(hexdec($a)) . "\'"; }'
-                                . $match[5] . PHP_EOL;
-                        echo 'NEW:' . PHP_EOL . trim($replace) . PHP_EOL;
-                    }
+                function ($match) {
+                    echo 'OLD: ' . PHP_EOL . trim($match[0]) . PHP_EOL;
+                    $replace = $match[1]
+                            . "preg_replace_callback('"
+                            . substr($match[3], 0, -1)
+                            . "',"
+                            . 'function ($a) { return "\'" . chr(hexdec($a)) . "\'"; }'
+                            . $match[5] . PHP_EOL;
+                    echo 'NEW:' . PHP_EOL . trim($replace) . PHP_EOL;
                 },
                 // additional transformations
                 // "/pattern/" => function ($match) { // code; },
