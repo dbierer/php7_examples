371f299f2532c071c94693087e8344c964b1eb7b
dbierer <garavatar@unlikelysource.com>
2015-11-23 13:36:36 +0700

    2015-11-23 DB: added json fractional part example

--- json_preserve_fractional_part.php
+++ json_preserve_fractional_part.php
@@ -1,20 +1,7 @@
 <?php
 // jsond vs. json extension
-
-$raw1 = (float) 100;
-$raw2 = (float) 100.01;
-echo "Raw values\n";
-var_dump($raw1, $raw2);
-$json1 = json_encode($raw1);
-$json2 = json_encode($raw2);
-echo "Json encoded\n";
-var_dump($json1, $json2);
-echo PHP_EOL;
-echo "Json decoded\n";
-var_dump(json_decode($json1), json_decode($json2));
-echo PHP_EOL;
-
 // https://wiki.php.net/rfc/json_preserve_fractional_part
+
 // // Currently
 echo json_encode(10.0); // Output 10
 echo PHP_EOL;
