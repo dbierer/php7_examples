a1f5807f11fa624e42bd0c1158a6d3315fd03959
dbierer <garavatar@unlikelysource.com>
2015-11-23 13:16:45 +0700

    2015-11-23

--- preg_replace_callback_array.php
+++ preg_replace_callback_array.php
@@ -7,26 +7,30 @@
 $lib = __DIR__ . '/phpLDAPadmin/lib/';
 $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($lib),
                                          RecursiveIteratorIterator::SELF_FIRST);
+$count = 0;
 foreach($objects as $name => $fileObj){
     // this is the filename
     if ($fileObj->isFile()) {
-        echo 'Processing: ' . $name . PHP_EOL;
         $contents = file($name);
-        preg_replace_callback_array(
-            [
-                "!.*(preg_replace)\(\'(.*?\/e)\',(\".*?\").*!" => function ($match) {
-                    echo $match[0], ' match found', PHP_EOL;
-                    echo $match[1], ' match found', PHP_EOL;
-                    echo $match[2], ' match found', PHP_EOL;
-                    return preg_replace([$match[1],$match[2],$match[3]],
-                                        ['preg_replace_callback',
-                                         substr($match[2], 0, -1),
-                                         function ($a) { return chr(hexdec($a));}], $match[0]);
-                },
-            ],
-            $contents, 
-        );
-
+        if (preg_replace('!/e!', $contents)) {
+            echo 'Processing: ' . $name . PHP_EOL;
+            preg_replace_callback_array(
+                [
+                    // 1   2                 3          4        5
+                    "!(.*)(preg_replace)\(\'(.*?\/e)\',(\".*?\")(.*)!" => function ($match) {
+                        return  $match[1] 
+                                . "preg_replace('" 
+                                . substr($match[3], 0, -1) 
+                                . "',"
+                                . function ($a) { return chr(hexdec($a)); }
+                                . $match[5] . PHP_EOL;
+                    },
+                ],
+                $contents, 
+            );
+            $fn = sprintf('%s/temp_%04d.php', __DIR__, $count++);
+            file_put_contents($fn, $contents);
+        }
     }
 }
 
