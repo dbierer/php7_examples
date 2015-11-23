5ab4ce5bc801972881754b5c0097b3a030e98bbd
dbierer <doug@unlikelysource.com>
2015-11-23 17:45:51 +0700

    2015-11-23 DB: preg_replace_array #3

--- preg_replace_callback_array.php
+++ preg_replace_callback_array.php
@@ -15,22 +15,23 @@ foreach($objects as $name => $fileObj){
         echo 'Processing: ' . $name . PHP_EOL;
         $flag = FALSE;
         preg_replace_callback_array(
-            [   "!preg_replace.*?\/e!" =>
-                function ($match) use (&$flag) { $flag = TRUE; },
+            [
                 // 1   2                 3          4        5
                 "!(.*)(preg_replace)\(\'(.*?\/e)\',(\".*?\")(.*)!" =>
                 function ($match) use ($flag) {
                     if ($flag) {
-                        echo 'OLD: ' . PHP_EOL . $match[0] . PHP_EOL;
+                        echo 'OLD: ' . PHP_EOL . trim($match[0]) . PHP_EOL;
                         $replace = $match[1]
                                 . "preg_replace_callback('"
                                 . substr($match[3], 0, -1)
                                 . "',"
                                 . 'function ($a) { return "\'" . chr(hexdec($a)) . "\'"; }'
                                 . $match[5] . PHP_EOL;
-                        echo 'NEW:' . PHP_EOL . $replace . PHP_EOL;
+                        echo 'NEW:' . PHP_EOL . trim($replace) . PHP_EOL;
                     }
                 },
+                // additional transformations
+                // "/pattern/" => function ($match) { // code; },
             ],
             $contents
         );
