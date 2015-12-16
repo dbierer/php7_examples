d870227c45e069a05a6363611a2ce8a2c7f7d87c
dbierer <doug@unlikelysource.com>
2015-12-16 11:16:59 +0700

    2015-12-16 DB 7

--- list_changes_explode.php
+++ list_changes_explode.php
@@ -2,18 +2,18 @@
 // TODO: test list() with explode()
 
 $string = 'TEST1 TEST2 TEST3';
-list($a, $b, $c) = expode(' ', $string);
+list($a, $b, $c) = explode(' ', $string);
 var_dump($a, $b, $c, $d);
 echo PHP_EOL;
 
-list($a, $b) = expode(' ', $string);
+list($a, $b) = explode(' ', $string);
 var_dump($a, $b, $c, $d);
 echo PHP_EOL;
 
-list($a, $b, $c, $d) = expode(' ', $string);
+list($a, $b, $c, $d) = explode(' ', $string);
 var_dump($a, $b, $c, $d);
 echo PHP_EOL;
 
-list($a, $b, $c, $d, $e) = expode(' ', $string);
+list($a, $b, $c, $d, $e) = explode(' ', $string);
 var_dump($a, $b, $c, $d);
 echo PHP_EOL;
