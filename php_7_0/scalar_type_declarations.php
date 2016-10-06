22fb5bfd10c573668b58abd6ced94f7ecb4b072a
doug <doug@unlikelysource.com>
2016-10-06 23:58:21 +0700

    2016-10-06 DB: updated scalar type declarations example

--- php_7_0/scalar_type_declarations.php
+++ php_7_0/scalar_type_declarations.php
@@ -6,7 +6,7 @@ function add(float $a, float $b): int {
 }
 
 try {
-    var_dump(add(1.5, 2.5)); // int(4)
+    var_dump(add(1.6, 2.6)); // int(4)
     // Notice: A non well formed numeric value encountered
     var_dump(add("1 foo", "2")); // int(3)
     add(1, 2); // Fatal error: Argument 1 passed to add() must be of the type float, integer given
