3491130a45b97eec21c498070e6b1ed9edbab549
Andrew Caya <andrewscaya@yahoo.ca>
2017-03-20 23:16:51 -0400

    2017-03-20 AC: Updates all Docker support files.

--- docker_run.bash
+++ docker_run.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd docs/dockerfiles
-gnome-terminal -e ./run-5.6.28.bash
-gnome-terminal -e ./run-7.0.13.bash
-gnome-terminal -e ./run-7.1.1.bash
+cd docs/docker
+gnome-terminal -e ./run-5.6.bash
+gnome-terminal -e ./run-7.0.bash
+gnome-terminal -e ./run-7.1.bash
 cd
 echo -e "***********************************************"
 echo -e "PHP 5.6 is running on port 5555\nPHP 7.0 is running on port 7777\nPHP 7.1 is running on port 9999\n"
