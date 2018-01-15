e2d7bd7f411515978205ad8f92cafbdb08bb312c
Andrew Caya <andrewscaya@yahoo.ca>
2018-01-15 18:52:05 -0500

    2017-12-18 AC: Updates scripts for use with ASCLinux (Linux for PHP) 8.0.

--- docs/docker/bin/deploy.bash
+++ docs/docker/bin/deploy.bash
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-cd /srv
-mv www www.OLD
-ln -s /www .
-sed -i 's/DirectoryIndex index.html/DirectoryIndex index.php index.html/' /etc/httpd/httpd.conf
 /etc/init.d/mysql start
 /etc/init.d/php-fpm start
 /etc/init.d/httpd start
--- docs/docker/run-7.2.bash
+++ docs/docker/run-7.2.bash
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 docker run --rm -it \
-  -v ${PWD}/../../:/www \
+  -v ${PWD}/../../:/srv/www \
   -p 7272:80 \
   -p 10446:443 \
   -p 13309:3306 \
-  -p 10025:22 \
-  asclinux/unlikelysource-php-7.2.0 \
-  /www/docs/docker/bin/deploy.bash run
+  asclinux/unlikelysource-php-7.2.0:8.0 \
+  /srv/www/docs/docker/bin/deploy.bash run
